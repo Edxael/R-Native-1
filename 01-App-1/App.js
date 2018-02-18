29596dea18a3d84824f452661319f92473036633
Edxael <zerphyr.r@gmail.com>
2018-02-18 01:26:53 -0700

    add notes

--- 01-App-1/App.js
+++ 01-App-1/App.js
@@ -12,8 +12,8 @@ export default class App extends Component{
   
           <Text>Open up App.js to start working on your app!</Text>
           <Text>Changes you make will automatically reload...</Text>
-          <Text>Shake your phone to open the developer menu.</Text>
-          <Text>Hello Edmundo this is a phone app.</Text>
+          <Text>Shake your phone to open the developer menu...</Text>
+          <Text>Hello Edmundo this is a phone app...</Text>
           
         </View>
       );
