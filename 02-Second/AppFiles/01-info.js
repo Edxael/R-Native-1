81ac3a10f6c7bf4b32555d2e5f3e0d748a2d2f98
Edxael <zerphyr.r@gmail.com>
2018-02-19 14:42:21 -0700

    changin states

--- 02-Second/AppFiles/01-info.js
+++ 02-Second/AppFiles/01-info.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class extends Component{
+
+    constructor(){
+        super()
+        this.state = {
+            show: false
+        }
+    }
     
     static defaultProps = {
         info: 'Hello Edxael'
+    }
+
+    exe1(){
+        console.log("Executing Function")
+        console.log("Show: ", this.state.show);
+        let store = (this.state.show) ? false : true
+        console.log("Store: ", store);
+        
+        this.setState({ show: store })
+        setTimeout(() => { console.log("Show: ", this.state.show) }, 500)
+    }
+
+    componentWillMount(){
+        this.exe1()
     }
     
     render(){
@@ -12,9 +33,13 @@ export default class extends Component{
         return(
       
             <View>
-                <Text>Hello From 01 File.**.01</Text>
+
+                <Text>Hello From 01 File.** 03 **</Text>
                 <Text>{ this.props.data }</Text>
                 <Text>{ this.props.info }</Text>
+
+                <Text> { (this.state.show) ? "We Are Open" : "We Are Closed" } </Text>
+              
 
             </View>
             
