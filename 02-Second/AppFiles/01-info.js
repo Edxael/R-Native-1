42fbc634c24b01e872b856bc96012e23af3f5f61
Edxael <zerphyr.r@gmail.com>
2018-02-19 14:21:14 -0700

    creating default props

--- 02-Second/AppFiles/01-info.js
+++ 02-Second/AppFiles/01-info.js
@@ -3,14 +3,19 @@ import { Text, View } from 'react-native'
 
 export default class extends Component{
     
+    static defaultProps = {
+        info: 'Hello Edxael'
+    }
     
     render(){
-        console.log("Hello ************ again");
+        console.log("Hello ******** again");
         return(
       
             <View>
-                <Text>Hello From 01 File..01</Text>
+                <Text>Hello From 01 File.**.01</Text>
                 <Text>{ this.props.data }</Text>
+                <Text>{ this.props.info }</Text>
+
             </View>
             
         )
